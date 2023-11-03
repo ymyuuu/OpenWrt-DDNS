ed6d4471527fd32b8cce0c388ad7b3ae25003e19
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2023-11-03 21:48:15 +0800

    Update update.py

--- update.py
+++ update.py
@@ -23,42 +23,60 @@ start_time_str = start_time.strftime('%Y-%m-%d %H:%M')
 print(f"\n{start_time_str} 正在下载更新反代IP库\n")
 
 # 下载ZIP文件
-response = requests.get(download_url)
-with open(zip_file_name, "wb") as zip_file:
-    zip_file.write(response.content)
+try:
+    response = requests.get(download_url)
+    response.raise_for_status()  # 检查是否有HTTP错误
+    with open(zip_file_name, "wb") as zip_file:
+        zip_file.write(response.content)
+except Exception as e:
+    print(f"下载ZIP文件时出现错误: {str(e)}")
+    response = None
 
 # 解压ZIP文件
-with zipfile.ZipFile(zip_file_name, "r") as zip_ref:
-    zip_ref.extractall(os.path.join(script_dir, "data_folder"))
+if response:
+    try:
+        with zipfile.ZipFile(zip_file_name, "r") as zip_ref:
+            zip_ref.extractall(os.path.join(script_dir, "data_folder"))
+    except Exception as e:
+        print(f"解压ZIP文件时出现错误: {str(e)}")
 
 # 读取并合并txt文件
 ip_set = set()
 for root, _, files in os.walk(os.path.join(script_dir, "data_folder")):
     for file in files:
         if file.endswith(".txt"):
-            with open(os.path.join(root, file), "r") as txt_file:
-                for line in txt_file:
-                    line = line.strip()
-                    if line:
-                        ip_set.add(line)
+            try:
+                with open(os.path.join(root, file), "r") as txt_file:
+                    for line in txt_file:
+                        line = line.strip()
+                        if line:
+                            ip_set.add(line)
+            except Exception as e:
+                print(f"读取并合并txt文件时出现错误: {str(e)}")
 
 # 读取上次的IP记录
 old_ip_list = []
 if not is_first_run:
-    with open(ip_txt_file_name, "r") as old_ip_file:
-        for line in old_ip_file:
-            line = line.strip()
-            if line:
-                old_ip_list.append(line)
+    try:
+        with open(ip_txt_file_name, "r") as old_ip_file:
+            for line in old_ip_file:
+                line = line.strip()
+                if line:
+                    old_ip_list.append(line)
+    except Exception as e:
+        print(f"读取上次的IP记录时出现错误: {str(e)}")
 
 # 检查新增和删除的IP
 added_ips = list(ip_set - set(old_ip_list))
 removed_ips = list(set(old_ip_list) - ip_set)
 
 # 保存新的IP记录
-with open(ip_txt_file_name, "w") as new_ip_file:
-    for ip in sorted(ip_set, key=lambda x: [int(part) for part in x.split('.')]):
-        new_ip_file.write(ip + '\n')
+try:
+    with open(ip_txt_file_name, "w") as new_ip_file:
+        for ip in sorted(ip_set, key=lambda x: [int(part) for part in x.split('.')]):
+            new_ip_file.write(ip + '\n')
+except Exception as e:
+    print(f"保存新的IP记录时出现错误: {str(e)}")
 
 # 输出更新信息
 end_time = datetime.now()
@@ -80,8 +98,11 @@ if not is_first_run:  # 只在不是第一次运行时输出变化信息
         print(f"IP库已是最新，共有 {count} 个IP\n")
 else:
     count = len(ip_set)
-    print(f"首次运行，已下载最新IP库，{count} 个IP\n")
+    print(f"首次运行，已下载最新IP库，共有 {count} 个IP\n")
 
 # 清理临时文件
-os.remove(zip_file_name)
-shutil.rmtree(os.path.join(script_dir, "data_folder"))
+try:
+    os.remove(zip_file_name)
+    shutil.rmtree(os.path.join(script_dir, "data_folder"))
+except Exception as e:
+    print(f"清理临时文件时出现错误: {str(e)}")
