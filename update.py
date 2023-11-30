616eddb50e9560302e3c74d4203b581dde3a7071
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2023-12-01 00:23:38 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,15 +1,12 @@
 import os
 import requests
-import zipfile
-import shutil
 from datetime import datetime
 
 # 获取脚本所在的目录
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # 定义下载URL和文件名
-download_url = "https://proxy-ip.030101.xyz"
-zip_file_name = os.path.join(script_dir, "data.zip")
+download_url = "https://proxy-ip.030101.xyz/iptxt"
 ip_txt_file_name = os.path.join(script_dir, "ip.txt")
 
 # 判断是否是第一次运行
@@ -22,89 +19,41 @@ start_time = datetime.now()
 start_time_str = start_time.strftime('%Y-%m-%d %H:%M')
 print(f"\n{start_time_str} 正在下载更新反代IP库\n")
 
-# 下载ZIP文件
+# 下载ip.txt文件，并跳过前三行
 try:
     response = requests.get(download_url)
     response.raise_for_status()  # 检查是否有HTTP错误
-    with open(zip_file_name, "wb") as zip_file:
-        zip_file.write(response.content)
+    with open(ip_txt_file_name, "wb") as txt_file:
+        lines = response.iter_lines()
+        next(lines)  # 跳过第一行
+        next(lines)  # 跳过第二行
+        next(lines)  # 跳过第三行
+        for line in lines:
+            txt_file.write(line + b'\n')
 except Exception as e:
-    print(f"下载ZIP文件时出现错误: {str(e)}")
+    print(f"下载ip.txt文件时出现错误: {str(e)}")
     response = None
 
-# 解压ZIP文件
+# 读取新的IP记录
+new_ip_list = []
 if response:
     try:
-        with zipfile.ZipFile(zip_file_name, "r") as zip_ref:
-            zip_ref.extractall(os.path.join(script_dir, "data_folder"))
-    except Exception as e:
-        print(f"解压ZIP文件时出现错误: {str(e)}")
-
-# 读取并合并txt文件
-ip_set = set()
-for root, _, files in os.walk(os.path.join(script_dir, "data_folder")):
-    for file in files:
-        if file.endswith(".txt"):
-            try:
-                with open(os.path.join(root, file), "r") as txt_file:
-                    for line in txt_file:
-                        line = line.strip()
-                        if line:
-                            ip_set.add(line)
-            except Exception as e:
-                print(f"读取并合并txt文件时出现错误: {str(e)}")
-
-# 读取上次的IP记录
-old_ip_list = []
-if not is_first_run:
-    try:
-        with open(ip_txt_file_name, "r") as old_ip_file:
-            for line in old_ip_file:
+        with open(ip_txt_file_name, "r") as new_ip_file:
+            for line in new_ip_file:
                 line = line.strip()
                 if line:
-                    old_ip_list.append(line)
+                    new_ip_list.append(line)
     except Exception as e:
-        print(f"读取上次的IP记录时出现错误: {str(e)}")
-
-# 检查新增和删除的IP
-added_ips = list(ip_set - set(old_ip_list))
-removed_ips = list(set(old_ip_list) - ip_set)
-
-# 保存新的IP记录
-try:
-    with open(ip_txt_file_name, "w") as new_ip_file:
-        for ip in sorted(ip_set, key=lambda x: [int(part) for part in x.split('.')]):
-            new_ip_file.write(ip + '\n')
-except Exception as e:
-    print(f"保存新的IP记录时出现错误: {str(e)}")
+        print(f"读取新的IP记录时出现错误: {str(e)}")
 
 # 输出更新信息
 end_time = datetime.now()
 start_time_str = start_time.strftime('%Y-%m-%d %H:%M')
-if response and not is_first_run:  # 只在不是第一次运行且没有下载错误时输出变化信息
-    if added_ips or removed_ips:
-        if added_ips:
-            print("+")
-            for ip in sorted(added_ips, key=lambda x: [int(part) for part in x.split('.')]):
-                print(ip)
-        if removed_ips:
-            print("-")
-            for ip in sorted(removed_ips, key=lambda x: [int(part) for part in x.split('.')]):
-                print(ip)
-        count = len(ip_set)
-        print(f"本次更新之后共有 {count} 个IP\n")
-    else:
-        count = len(ip_set)
-        print(f"IP库已是最新，共有 {count} 个IP\n")
-elif response and is_first_run:  # 只在不是第一次运行且没有下载错误时输出首次运行信息
-    count = len(ip_set)
-    print(f"首次运行，已下载最新IP库，共有 {count} 个IP\n")
-elif not response:
-    print("由于下载ZIP文件时出现错误，无法进行IP库更新。\n")
-
-# 清理临时文件
-try:
-    os.remove(zip_file_name)
-    shutil.rmtree(os.path.join(script_dir, "data_folder"))
-except Exception as e:
-    print(f"清理临时文件时出现错误: {str(e)}")
+if response:  
+    count = len(new_ip_list)
+    if not is_first_run:  
+        print(f"已更新IP库，共有 {count} 个IP\n")
+    else:  
+        print(f"首次运行，已下载最新IP库，共有 {count} 个IP\n")
+else:  
+    print("由于下载ip.txt文件时出现错误，无法进行IP库更新。\n")
